9381a4a9353152b25c8802c796a325235f9aed40
rukkro <rukkro@users.noreply.github.com>
2018-08-31 15:42:15 -0500

    Rename stuff, remove some redundancy

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 # This script does not use secure protocols
 from telnetlib import Telnet
-import getpass, socket, time
+import getpass, socket
 
 
-class teleCisc:
+class TeleCisc:
+
     PORT = 23
     
     def __init__(self):
@@ -13,55 +14,58 @@ class teleCisc:
         self.mode = ""
 
     @staticmethod
-    def input_passwd():
+    def input_password():
         passwd = ""
         while not passwd:
-            # getpass() does not work in normal PyCharm run mode, use debug mode
+            # getpass() does not work in normal IDE, use debug mode or the command line
             passwd = getpass.getpass('Password: ')
         return passwd
 
-    def user_input_cli(self):
+    def initial_connect(self):
         while not self.host:
             self.host = input("IP or Hostname: ")
         while not self.username:
             self.username = input("Username: ")
 
-    def device_login(self):
-        def get_password():
-            return self.input_passwd()
+    def ios_read(self):
         while True:
             login = self.tn.read_until(b"\n",timeout=2)
+            ### LOGIN STUFF ###
             if "Username:" in login.decode():
                 self.tn.write(self.username.encode('ascii') + b"\n")
                 self.tn.interact()
                 continue
             elif "Password:" in login.decode():
-                self.tn.write(get_password().encode('ascii') + b"\n")
+                self.tn.write(self.input_password().encode('ascii') + b"\n")
                 self.tn.interact()
                 continue
             elif "% Bad" in login.decode():
                 print("Bad Password!")
                 continue
+            ### MODE STUFF ###
+            # TODO: Verify modes are working fine, edge cases
             elif ">" in login.decode():
                 self.mode = "unprivileged"
-                passwd = None
+                break
+            elif "#" in login.decode():
+                self.mode = "unprivileged"
                 break
             else:
                 continue
 
     def connect(self):
-        self.user_input_cli()
-
+        self.initial_connect()
         print("Attempting connection to " + self.host + "...")
         try:
             self.tn = Telnet(self.host,self.PORT,timeout=10)
         except socket.gaierror as e:
+            # Kill connection when it fails
             print("Connection to host failed:",e)
             self.tn = None
             quit()
         print("Connection Succeeded!")
-        self.device_login()
+        self.ios_read()
         self.tn.interact()
 
 
-teleCisc().connect()
+TeleCisc().connect()
