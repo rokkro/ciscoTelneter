9f7e470126e6b9c41177e3294164cd3e38a6f1f1
rukkro <rukkro@users.noreply.github.com>
2018-09-04 16:47:05 -0500

    Work on saving confs

--- main.py
+++ main.py
@@ -1,34 +1,39 @@
-# This script does not use secure protocols
+# This script does not use secure protocols, as it is used in an education environment.
 from telnetlib import Telnet
 import getpass, socket
 
 
 class TeleCisc:
-
     PORT = 23
+    STORE_PASSWD = True
+    PRIVILEGED = "privileged"
+    UNPRIVILEGED = "unprivileged"
     IOS_SYNTAX = {
-        "username" : "Username:",
-        "password" : "Password:",
-        "login_fail" : "% Bad",
-        "unprivileged" : ">",
-        "privileged" : "#",
+        "username": "Username:",
+        "password": "Password:",
+        "login_fail": "% Bad",
+        "unprivileged": ">",
+        "privileged": "#",
+        "more": "--More--"
     }
-    
+
     def __init__(self):
         self.username = ""
         self.host = ""
-        self.password = ""  # Only used if store_passwd param is True in input_password()
+        self.password = ""  # Only used if STORE_PASSWD is True
         self.connection = None
         self.mode = ""
+        self.running_config = []
+        self.startup_config = []
 
-    def input_password(self, store_passwd):
+    def input_password(self):
         passwd = ""
-        if store_passwd and self.password:
+        if self.STORE_PASSWD and self.password:
             return self.password
         while not passwd:
             # getpass() does not work in normal IDE, use debug mode or the command line
             passwd = getpass.getpass('Password: ')
-        if store_passwd:
+        if self.STORE_PASSWD:
             self.password = passwd
         return passwd
 
@@ -38,8 +43,24 @@ class TeleCisc:
         while not self.username:
             self.username = input("Username: ")
 
-    def ios_store_running_conf(self):
-        pass
+    def ios_fetch_and_store_conf(self):
+        if not self.PRIVILEGED:
+            self.ios_read()
+        print("Reading running-config and startup-config...")
+        def read_conf(list, file_name):
+            self.connection.write(("show " + file_name).encode("ascii") + b"\n")
+            line = self.connection.read_until(b"\n", timeout=5).decode().strip()
+            while line.strip():
+                line = self.connection.read_until(b"\n", timeout=5).decode().strip()
+                if self.IOS_SYNTAX["more"] in line:
+                    self.connection.write(b" ")
+                    continue
+                list.append(line)
+                print(line)
+
+        read_conf(self.startup_config,"startup-config")
+        read_conf(self.running_config,"running-config")
+
 
     def ios_read(self):
         while True:
@@ -52,19 +73,19 @@ class TeleCisc:
                 self.connection.interact()
                 continue
             elif self.IOS_SYNTAX["password"] in line.decode():
-                self.connection.write(self.input_password(True).encode('ascii') + b"\n")
+                self.connection.write(self.input_password().encode('ascii') + b"\n")
                 continue
             elif self.IOS_SYNTAX["login_fail"] in line.decode():
                 print("Bad Password!")
                 continue
             ### MODE STUFF ###
             elif self.IOS_SYNTAX["unprivileged"] in line.decode():
-                self.mode = "unprivileged"
+                self.mode = self.UNPRIVILEGED
                 print("Logged in... Entering Privileged Mode...")
                 self.connection.write("enable".encode("ascii") + b"\n")
                 continue
             elif self.IOS_SYNTAX["privileged"] in line.decode():
-                self.mode = "unprivileged"
+                self.mode = self.PRIVILEGED
                 print("Entered Privileged Mode.")
                 break
             else:
@@ -77,12 +98,15 @@ class TeleCisc:
             self.connection = Telnet(self.host, self.PORT, timeout=10)
         except socket.gaierror as e:
             # Kill connection when it fails
-            print("Connection to host failed:",e)
+            print("Connection to host failed:", e)
             self.connection = None
             quit()
         print("Connection Succeeded! Waiting for log in prompt...")
-        self.ios_read()
-        self.connection.interact()
-
+        try:
+            self.ios_read()
+            self.ios_fetch_and_store_conf()
+            self.connection.interact()
+        except ConnectionAbortedError as e:
+            print("Telnet connection died:",e)
 
 TeleCisc().connect()
