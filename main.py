840f3d02fef433177745bf8a86e5266a12445f0f
rukkro <rukkro@users.noreply.github.com>
2018-09-07 10:50:08 -0500

    Nicer initial message. Display config file selection menu, print out selected file contents

--- main.py
+++ main.py
@@ -1,12 +1,114 @@
-# This script does not use secure protocols, as it is used in an education environment.
+# DO NOT USE THIS SCRIPT IN PLACES WHERE SECURITY IS IMPORTANT! Telnet is NOT a secure protocol.
+# This script is used in an isolated student/education environment.
+# It serves as a simple way for a lab assistant to reset device configurations after students do lab assignments.
+# Used for resetting Cisco IOS 12.X switches/routers.
+# This script is not yet fully automatic at the moment,since a new config file needs to be selected using the menu UI.
+# Running this in an IDE will probably not display the password prompt, given how getpass() works.
 from telnetlib import Telnet
-import getpass, socket
+import getpass, socket, os
+
+# PUT THE STARTING DIRECTORY FOR LOCATING CONFIG FILES HERE
+# Tip: you can use forward slashes instead of backslashes on Windows
+CONFIGS_ROOT_DIR = ""
+
+
+class Menu:
+    colors = {
+        "purple": "\033[1;94m",
+        "yellow": '\33[33m',
+        "end": '\033[1;0m',
+    }
+    horizontal_len = 40
+    key_queue = []
+
+    def header(self, text):  # ---header text---
+        print(self.colors['end'] + self.colors['yellow'] + ('-' * int((self.horizontal_len - len(text)) / 2)) +
+              self.colors['end'] +
+              text + self.colors['end'] + self.colors['yellow'] + ('-' * int((self.horizontal_len - len(text)) / 2)) +
+              self.colors['end'])
+
+    def divider(self):  # ----------
+        print(self.colors['end'] + self.colors['yellow'] + '-' * self.horizontal_len + self.colors['end'])
+
+    def get_menu(self, head, menu, input_menu):
+        # Numbered user input menu
+        while True:
+            if not menu:
+                print("There doesn't appear to be anything here...")
+                return 'r'
+            if menu is not None:
+                self.header(head)
+                for num, entry in enumerate(menu):  # Print entries
+                    print("[" + self.colors['purple'] + str(num + 1) + self.colors['end'] + "] - " + str(entry))
+                self.divider()
+            if not self.key_queue:
+                # Stylize input menu
+                entry = input(self.colors['end'] + input_menu.replace("[", self.colors['end'] +
+                                                                      "[" + self.colors['purple']).replace("]",
+                                                                                                           self.colors[
+                                                                                                               'end'] + "]" +
+                                                                                                           self.colors[
+                                                                                                               'end'])).strip()
+                if len(entry.split(" ")) > 1:
+                    entries = entry.split(" ")
+                    entry = entries[0]
+                    del entries[0]
+                    self.key_queue.extend(entries)
+            else:
+                entry = self.key_queue[0]
+                del self.key_queue[0]
+            if entry == 'q':  # input 'q' to quit
+                quit()
+            elif entry == 'r' or entry == '':  # Returns r or space for menus to handle it.
+                return entry
+            try:  # Type cast num input to int
+                entry = int(entry)
+            except ValueError:
+                continue
+            if ((entry > len(menu)) if menu is not None else False) or entry < 1:
+                # (Compare entry to menu size if the menu exists, otherwise False) OR if input is < 1
+                continue  # Recognize as invalid input
+            return entry  # Successfully return input for menus to handle
+
+    def get_path_menu(self, path="./"):
+        """
+        Saves original working dir in cwd. Gets abs path of input dir, and changes to it.
+          Creates menu from that dir. If ENTER is pressed, go up a dir. If a dir is selected, change to it.
+          Rinse and Repeat. Convert final file path to abspath, and cut out file name from it. Change to original cwd.
+          Return absolute path and the file name (strings).
+        """
+        cwd = os.getcwd()
+
+        def set_file(path):
+            path = os.path.abspath(path)
+            os.chdir(path)
+            while True:
+                files = os.listdir("./")
+                inpt = self.get_menu("PATH", files, "*Select a file or [Enter] - go up a dir, [r] - return.\n>>>")
+                if inpt == 'r':
+                    return None
+                elif inpt == '':
+                    os.chdir("..")
+                    continue
+                selected = files[inpt - 1]
+                if os.path.isdir("./" + selected):
+                    os.chdir(selected)
+                    continue
+                return selected
+
+        selected_file = set_file(path)
+        if selected_file is None:
+            return
+        file_abs = os.path.abspath(selected_file).replace(selected_file, "")
+        os.chdir(cwd)
+        return file_abs, selected_file
 
 
 class TeleCisc:
     PORT = 23
     STORE_PASSWD = True
     READ_TIMEOUT = 3
+    CONNECT_TIMEOUT = 10
     PRIVILEGED = "privileged"
     UNPRIVILEGED = "unprivileged"
     IOS_SYNTAX = {
@@ -61,9 +163,9 @@ class TeleCisc:
                 line = line.strip().decode()
                 store_list.append(line)
 
-        read_conf(self.startup_config,"startup-config")
-        read_conf(self.running_config,"running-config")
-        print(self.running_config,self.startup_config)
+        read_conf(self.startup_config, "startup-config")
+        read_conf(self.running_config, "running-config")
+        print(self.running_config, self.startup_config)
 
     def ios_read(self):
         while True:
@@ -97,10 +199,13 @@ class TeleCisc:
                 continue
 
     def connect(self):
+        abs_path, file_name = Menu().get_path_menu(CONFIGS_ROOT_DIR)
+        print(list(i.strip() for i in open(abs_path + file_name)))
+
         self.initial_connect()
         print("Attempting connection to " + self.host + "...")
         try:
-            self.connection = Telnet(self.host, self.PORT, timeout=10)
+            self.connection = Telnet(self.host, self.PORT, timeout=self.CONNECT_TIMEOUT)
         except socket.gaierror as e:
             # Kill connection when it fails
             print("Connection to host failed:", e)
@@ -111,8 +216,9 @@ class TeleCisc:
             self.ios_read()
             self.ios_fetch_and_store_conf()
         except ConnectionAbortedError as e:
-            print("Telnet connection died:",e)
+            print("Telnet connection died:", e)
         except EOFError as e:
-            print("Telnet connection died:",e)
+            print("Telnet connection died:", e)
+
 
 TeleCisc().connect()
